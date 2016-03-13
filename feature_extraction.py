47602548fd8d0a02e95365c6a6c3c908e179d021
Chua Jie Sheng <chuajiesheng@gmail.com>
2016-03-13 22:23:28 +0800

    Add matrix for feature generation

--- feature_extraction.py
+++ feature_extraction.py
@@ -5,6 +5,8 @@ from xml import parsers
 import threading
 import bug
 import sys
+import numpy as np
+import scipy.sparse as sps
 
 DATA_DIRECTORY = 'data/huge-eclipse-xml-reports'
 bugs = []
@@ -33,9 +35,9 @@ def parse_file(file_path):
     for item in itemlist:
         b = bug.Bug(item)
         if b is not None:
-            # bugs.append(bug)
+            bugs.append(b)
             # print b.to_csv()
-            print b.to_short_desc_csv()
+            # print b.to_short_desc_csv()
 
 
 def multithread_parse_file(file_path):
@@ -45,6 +47,152 @@ def multithread_parse_file(file_path):
 
 if __name__ == '__main__':
     files = [f for f in listdir(DATA_DIRECTORY) if isfile(join(DATA_DIRECTORY, f))]
+    files = ['bugs000101-000200.xml']
     for f in files:
         file_path = join(DATA_DIRECTORY, f)
         parse_file(file_path)
+
+    # row = all bugs length
+    # column = features
+    features = sps.lil_matrix((len(bugs), 43), dtype=np.longdouble)
+
+    bug_severity_list = [(b.bug_severity, b.creation_ts) for b in bugs]
+
+    for i in range(len(bugs)):
+        col_index = 0
+        b = bugs[i]
+
+        features[i, col_index] = b.num_of_comments(5)
+        col_index += 1
+
+        features[i, col_index] = b.num_of_bugs(bug_severity_list, 7)
+        col_index += 1
+
+        features[i, col_index] = b.num_of_bugs_with_severity(bug_severity_list, 7)
+        col_index += 1
+
+        features[i, col_index] = b.num_of_bugs_with_same_or_higher_severity(bug_severity_list, 5)
+        col_index += 1
+
+        # TODO: Textual Factor
+
+        features[i, col_index] = b.mean_priority_of_author(bugs)
+        col_index += 1
+
+        features[i, col_index] = b.median_priority_of_author(bugs)
+        col_index += 1
+
+        features[i, col_index] = b.mean_priority_of_bug_by_author_prior(bugs)
+        col_index += 1
+
+        features[i, col_index] = b.median_priority_of_bug_by_author_prior(bugs)
+        col_index += 1
+
+        features[i, col_index] = b.num_of_bug_by_author_prior(bugs)
+        col_index += 1
+
+        features[i, col_index] = b.os()
+        col_index += 1
+
+        # TODO: cosine similarity
+
+        features[i, col_index] = b.mean_priority_of_top20
+        col_index += 1
+
+        features[i, col_index] = b.median_priority_of_top20
+        col_index += 1
+
+        features[i, col_index] = b.mean_priority_of_top10
+        col_index += 1
+
+        features[i, col_index] = b.median_priority_of_top10
+        col_index += 1
+
+        features[i, col_index] = b.mean_priority_of_top5
+        col_index += 1
+
+        features[i, col_index] = b.median_priority_of_top5
+        col_index += 1
+
+        features[i, col_index] = b.mean_priority_of_top3
+        col_index += 1
+
+        features[i, col_index] = b.median_priority_of_top3
+        col_index += 1
+
+        features[i, col_index] = b.mean_priority_of_top1
+        col_index += 1
+
+        features[i, col_index] = b.median_priority_of_top1
+        col_index += 1
+
+        # TODO: end cosine similarity
+
+        features[i, col_index] = b.severity_index()
+        col_index += 1
+
+        features[i, col_index] = b.product_feature()
+        col_index += 1
+
+        features[i, col_index] = b.num_of_bug_for_same_product_prior(bugs)
+        col_index += 1
+
+        features[i, col_index] = b.num_of_bug_for_same_product_and_severity_prior(bugs)
+        col_index += 1
+
+        features[i, col_index] = b.num_of_bug_for_same_product_and_same_or_higher_severity_prior(bugs)
+        col_index += 1
+
+        features[i, col_index] = b.pro_of_p1_bug_with_same_priority_and_prior(bugs)
+        col_index += 1
+
+        features[i, col_index] = b.pro_of_p2_bug_with_same_priority_and_prior(bugs)
+        col_index += 1
+
+        features[i, col_index] = b.pro_of_p3_bug_with_same_priority_and_prior(bugs)
+        col_index += 1
+
+        features[i, col_index] = b.pro_of_p4_bug_with_same_priority_and_prior(bugs)
+        col_index += 1
+
+        features[i, col_index] = b.pro_of_p5_bug_with_same_priority_and_prior(bugs)
+        col_index += 1
+
+        features[i, col_index] = b.mean_priority_of_bug_for_same_product_prior(bugs)
+        col_index += 1
+
+        features[i, col_index] = b.median_priority_of_bug_for_same_product_prior(bugs)
+        col_index += 1
+
+        features[i, col_index] = b.component_feature()
+        col_index += 1
+
+        features[i, col_index] = b.num_of_bug_for_same_component_prior(bugs)
+        col_index += 1
+
+        features[i, col_index] = b.num_of_bug_for_same_component_and_severity_prior(bugs)
+        col_index += 1
+
+        features[i, col_index] = b.num_of_bug_for_same_component_and_same_or_higher_severity_prior(bugs)
+        col_index += 1
+
+        features[i, col_index] = b.pro_of_p1_bug_with_same_priority_and_prior_for_component(bugs)
+        col_index += 1
+
+        features[i, col_index] = b.pro_of_p2_bug_with_same_priority_and_prior_for_component(bugs)
+        col_index += 1
+
+        features[i, col_index] = b.pro_of_p3_bug_with_same_priority_and_prior_for_component(bugs)
+        col_index += 1
+
+        features[i, col_index] = b.pro_of_p4_bug_with_same_priority_and_prior_for_component(bugs)
+        col_index += 1
+
+        features[i, col_index] = b.pro_of_p5_bug_with_same_priority_and_prior_for_component(bugs)
+        col_index += 1
+
+        features[i, col_index] = b.mean_priority_of_bug_for_same_component_prior(bugs)
+        col_index += 1
+
+        features[i, col_index] = b.median_priority_of_bug_for_same_component_prior(bugs)
+        col_index += 1
