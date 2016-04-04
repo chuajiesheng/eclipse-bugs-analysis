407458863f6ca59b1e2d7470143f2c12855b5849
Chua Jie Sheng <chuajiesheng@gmail.com>
2016-04-05 01:04:38 +0800

    Add py to download bug report

---
+++ download_bug_reports.py
@@ -0,0 +1,28 @@
+import urllib2
+import sys
+
+START = 193701
+# STOP = 491000
+STOP = 194000
+STEP = 100
+
+URL = 'https://bugs.eclipse.org/bugs/show_bug.cgi'
+CONTENT_TYPE = 'application/x-www-form-urlencoded'
+
+PRE_BODY = 'ctype=xml'
+POST_BODY = 'excludefield=attachmentdata'
+
+FILENAME = 'data/huge-eclipse-xml-reports/bugs{}-{}.xml'
+
+for i in range(START, STOP, STEP):
+    ids = 'id=' + '&id='.join([str(x) for x in range(i, i + STEP, 1)])
+    data = PRE_BODY + '&' + ids + '&' + POST_BODY
+
+    req = urllib2.Request(URL, data)
+    response = urllib2.urlopen(req)
+
+    f = open(FILENAME.format(str(i), str(i + STEP - 1)), 'w+')
+    f.write(response.read())
+
+    sys.stdout.write('.')
+    sys.stdout.flush()
